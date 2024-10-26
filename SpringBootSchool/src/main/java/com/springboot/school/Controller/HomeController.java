76d02bb5b859756453c8a04b0a0a626c41cc81b9
ganeshwala <ganeshwalachintan@gmail.com>
2024-10-26 13:48:05 -0400

    Implemented Home and Course page with Home Controller & WebMvcConfigure

---
+++ SpringBootSchool/src/main/java/com/springboot/school/Controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.springboot.school.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(value= {"","/","home"})
+	public String displayHomePage() {
+		return "home.html";
+	}
+}

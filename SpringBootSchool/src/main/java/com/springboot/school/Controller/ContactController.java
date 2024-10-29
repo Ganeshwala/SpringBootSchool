238c7e156f8afd877787a40fd24a02e858cbc56c
ganeshwala <ganeshwalachintan@gmail.com>
2024-10-29 03:04:05 -0400

    @Valid Annotation

--- SpringBootSchool/src/main/java/com/springboot/school/Controller/ContactController.java
+++ SpringBootSchool/src/main/java/com/springboot/school/Controller/ContactController.java
@@ -4,6 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +15,7 @@ import org.springframework.web.servlet.ModelAndView;
 import com.springboot.school.entity.Contact;
 import com.springboot.school.services.ContactService;
 
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -23,7 +27,8 @@ public class ContactController {
 	private ContactService contactService;
 	
 	@RequestMapping("/contact")
-	public String displayContactPage() {
+	public String displayContactPage(Model model) {
+		model.addAttribute("contact", new Contact());  // by doing this i will let spring-Boot know that we pass fields Name and also I apply validation on field and than need to apply on UI side as well
 		return "contact.html";
 	}
 	
@@ -38,13 +43,18 @@ public class ContactController {
 	}*/
 	
 	@PostMapping("/saveMsg")
-	public ModelAndView saveMessage(Contact contact) {
+	public String saveMessage(@Valid @ModelAttribute("contact") Contact contact,Errors errors) {
+		
+		if(errors.hasErrors()){
+            log.error("Contact form validation failed due to : " + errors.toString());
+            return "contact.html";
+        }
 		log.info("Name "+contact.getName());
 		log.info("Mobile Number"+contact.getMobileNum());
 		log.info("Email "+contact.getEmail());
 		log.info("Subject "+ contact.getSubject());
 		log.info("Message "+contact.getMessage());
 		contactService.saveContactMessage(contact);
-		return new ModelAndView("redirect:/contact");
+		return "redirect:/contact";
 	}
 }

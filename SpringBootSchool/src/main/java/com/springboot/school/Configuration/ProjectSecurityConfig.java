fd0961ebbc23857adc0ef1231951cb4706a69cbd
ganeshwala <ganeshwalachintan@gmail.com>
2024-11-07 22:08:40 -0500

    In Memory User Details Manager

--- SpringBootSchool/src/main/java/com/springboot/school/Configuration/ProjectSecurityConfig.java
+++ SpringBootSchool/src/main/java/com/springboot/school/Configuration/ProjectSecurityConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,9 +14,11 @@ public class ProjectSecurityConfig {
 
 	@Bean
 	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity httpSecurity) throws Exception{
-		httpSecurity.authorizeHttpRequests(
-				request -> request.requestMatchers("/").permitAll()
-				.requestMatchers("/home").permitAll()
+		httpSecurity
+		.csrf(csrf->csrf.disable())
+		.authorizeHttpRequests(
+				request -> request.requestMatchers("/dashborad").authenticated()
+				.requestMatchers("/","/home").permitAll()
 				.requestMatchers("/contact").permitAll()
 				.requestMatchers("/courses").permitAll()
 				.requestMatchers("/saveMsg").permitAll()
@@ -21,8 +26,27 @@ public class ProjectSecurityConfig {
 				.requestMatchers("/holidays/**").permitAll()
 				.requestMatchers("/assets/**").permitAll()
 				)
-				.formLogin(Customizer.withDefaults())
+		.formLogin(loginConfigurer -> loginConfigurer.loginPage("/login")
+                .defaultSuccessUrl("/dashboard").failureUrl("/login?error=true").permitAll())
+        .logout(logoutConfigurer -> logoutConfigurer.logoutSuccessUrl("/login?logout=true")
+                .invalidateHttpSession(true).permitAll())
 				.httpBasic(Customizer.withDefaults());
 		return httpSecurity.build();
 	}
+	
+	@Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("12345")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("54321")
+                .roles("USER", "ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
 }

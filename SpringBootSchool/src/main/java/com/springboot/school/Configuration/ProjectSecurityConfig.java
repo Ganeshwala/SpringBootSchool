ee0ee1a920466e96b6431624705608b10ce992bb
ganeshwala <ganeshwalachintan@gmail.com>
2024-11-07 19:55:52 -0500

    Provide Security to individual URL using requestMatchers method

--- SpringBootSchool/src/main/java/com/springboot/school/Configuration/ProjectSecurityConfig.java
+++ SpringBootSchool/src/main/java/com/springboot/school/Configuration/ProjectSecurityConfig.java
@@ -11,8 +11,16 @@ public class ProjectSecurityConfig {
 
 	@Bean
 	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity httpSecurity) throws Exception{
-		httpSecurity.authorizeHttpRequests(request -> 
-				request.anyRequest().permitAll())
+		httpSecurity.authorizeHttpRequests(
+				request -> request.requestMatchers("/").permitAll()
+				.requestMatchers("/home").permitAll()
+				.requestMatchers("/contact").permitAll()
+				.requestMatchers("/courses").permitAll()
+				.requestMatchers("/saveMsg").permitAll()
+				.requestMatchers("/about").permitAll()
+				.requestMatchers("/holidays/**").permitAll()
+				.requestMatchers("/assets/**").permitAll()
+				)
 				.formLogin(Customizer.withDefaults())
 				.httpBasic(Customizer.withDefaults());
 		return httpSecurity.build();
